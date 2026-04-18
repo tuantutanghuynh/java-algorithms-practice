27515dbeb1d2bedf74009758198ba86e06a704e7
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-18 09:44:07 +0700

    feat(school-system): add SchoolSystem/src/schoolsystem/PrimaryTeacher.java

---
+++ SchoolSystem/src/schoolsystem/PrimaryTeacher.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package schoolsystem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tangh
+ */
+public class PrimaryTeacher extends Teacher {
+
+    int classLevel;
+
+    @Override
+    public void Input() {
+        Scanner sc = new Scanner(System.in);
+        super.Input();
+        
+        while(true){
+            System.out.print("Input class level (1-5): ");
+            try{
+                classLevel = Integer.parseInt(sc.nextLine().trim());
+                if(classLevel < 1 || classLevel >5){
+                    throw new Exception("Class level must be between 1 and 5");
+                }
+                break;
+            }catch(NumberFormatException e){
+                System.out.println("Error: " + e.getMessage());
+            }catch(Exception e){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+    }
+
+    @Override
+    String GetLevel() {
+        return "Primary (Gr." + classLevel + ")";
+    }
+    
+    @Override
+    public double CalcBonus(){
+        return salary * 0.15;
+    }
+    
+    @Override
+    public void PrintInfo(){
+        System.out.println("PRIMARY TEACHER: ");
+        super.Input();
+    }
+}
