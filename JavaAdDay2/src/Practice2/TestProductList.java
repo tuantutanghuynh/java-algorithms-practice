e15262f6840e1c3f4b7e476a7a9eae3c65ea36eb
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-22 21:32:41 +0700

    test: add Practice2 TestProductList

---
+++ JavaAdDay2/src/Practice2/TestProductList.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practice2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tangh
+ */
+public class TestProductList {
+
+    public static void main(String[] args) {
+        ProductArray pa = new ProductArray();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("===== PRODUCT MENU =====");
+            System.out.println("1. Add");
+            System.out.println("2. Show");
+            System.out.println("3. Write to file");
+            System.out.println("4. Read from file");
+            System.out.println("5. Exit");
+            System.out.print("Choose: ");
+
+            String choice = sc.nextLine().trim();
+
+            switch (choice) {
+                case "1":
+                    pa.Add();
+                    break;
+                case "2":
+                    pa.Show();
+                    break;
+                case "3":
+                    pa.WriteToFile();
+                    System.out.println("Write file successfully");
+                    break;
+                case "4":
+                    pa.ReadFromFile();
+                    System.out.println("Read file successfully");
+                    break;
+                case "5":
+                    System.out.println("Exit program");
+                    return;
+                default:
+                    System.out.println("Please choose 1-5");
+            }
+        }
+    }
+}
