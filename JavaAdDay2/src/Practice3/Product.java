d7b3c431170b430caf8361d37e3d5c5e8d96b6ee
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-27 22:45:34 +0700

    feat(Practice3): add Product model

---
+++ JavaAdDay2/src/Practice3/Product.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practice3;
+
+/**
+ *
+ * @author tangh
+ */
+public class Product {
+    String id, name;
+    float price;
+
+    public Product(String id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + price + ", " ;
+    }
+    
+}
