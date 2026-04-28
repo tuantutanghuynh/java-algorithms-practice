fbb913c58ffa6306412f93d553dcd90d3cee40d1
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-28 22:35:30 +0700

    Update JavaAdDay5/src/Connect/ConnectDB.java

---
+++ JavaAdDay5/src/Connect/ConnectDB.java
@@ -0,0 +1,28 @@
+package connect;
+
+import java.sql.*;
+
+public class ConnectDB {
+
+    private static Connection connection;
+
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=EmployeeDB";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "tuantu209423";
+
+    private ConnectDB() {
+    }
+
+    public static synchronized Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } else {
+                System.out.println("Already connect");
+            }
+        } catch (Exception e) {
+            System.out.println("Error connect: " + e.getMessage());
+        }
+        return connection;
+    }
+}
