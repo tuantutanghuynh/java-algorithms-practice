b3ce5782f20099d0ccf905a36fedddb18daf3eb1
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-05-07 20:52:38 +0700

    JavaAdDay7: add model/User.java

---
+++ JavaAdDay7/src/model/User.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author tangh
+ */
+public class User {
+    public int id;
+    public String username;
+    public String password;
+    public String role;
+    public boolean status;
+}
